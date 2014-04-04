fbf196471d098e57da6d33348f1bdc48f1407a9b
Nicholas Van de walle <nicholas@niceguys.co>
2014-04-04 13:20:21 -0700

    Just toggling things around

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express"),
     app     = express(),
-    port    = process.env.PORT || 4567;
+    port    = parseInt(process.env.PORT, 10) || 4567;
     
 app.get("/", function(req, res) {
   res.redirect("/index.html");
